7039d60bec911a0c245e82e46b3b433fb209ff8e
KokiHirokawa <kitakitsune1110@gmail.com>
2023-02-04 23:01:30 +0900

    Try API request

--- shared/src/commonMain/kotlin/com/example/myapplication/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/myapplication/Greeting.kt
@@ -1,10 +1,33 @@
 package com.example.myapplication
 
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
-    fun greet(): String {
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    @Throws(Exception::class)
+    suspend fun greeting(): String {
+        val rockets: List<RocketLaunch> = httpClient
+            .get("https://api.spacexdata.com/v4/launches")
+            .body()
+        val lastSuccessLaunch = rockets.last { it.launchSuccess == true }
         return "Guess what it is! > ${platform.name.reversed()}!" +
-                "\nThere are only ${daysUntilNewYear()} days left until New Year!"
+                "\nThere are only ${daysUntilNewYear()} days left until New Year!" +
+                "\nThe last successful launch was ${lastSuccessLaunch.launchDateUTC} 🚀"
     }
 }
